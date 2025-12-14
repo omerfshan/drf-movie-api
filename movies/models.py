5441307b843fd6501ae113ef5792571cc66228ee
sahan <fs.sahan09@gmail.com>
2025-12-14 07:06:35 +0300

    create models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+Category_Choices = [
+    ("movie", "Sinema Filmi"),
+    ("series", "Dizi"),
+    ("documentary", "Belgesel"),
+]
+
+class Movies(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    duration = models.PositiveIntegerField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    poster = models.CharField(max_length=255, null=True, blank=True)
+    category = models.CharField(max_length=20, choices=Category_Choices, default="movie")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
